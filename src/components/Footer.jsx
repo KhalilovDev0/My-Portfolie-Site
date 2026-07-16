e1f4613b5bca307392c9834079d62cacc0fee8dc
Appleex0 <agakhalilov@outlook.com>
2026-07-16 16:20:52 +0400

    fix: update GitHub URL in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     {
       id: "github",
       icon: <FaGithub />,
-      url: "https://github.com/Appleex0",
+      url: "https://github.com/KhalilovDev0",
       label: "GitHub Profile",
     },
     {
@@ -33,27 +33,29 @@ function Footer() {
   return (
     <footer className="w-full bg-white/10 backdrop-blur-md border-t border-white/5 text-white shadow-xl relative z-10">
       <div className="max-w-8xl mx-auto flex flex-col sm:flex-row justify-between items-center gap-4 py-8 px-4 sm:px-6 lg:px-8">
-      
-      <ul className="text-lg flex gap-6 items-center">
-        {socialLinks.map((link) => (
-          <li key={link.id}>
-            <a
-              href={link.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={link.label}
-              className="block text-slate-400 hover:text-cyan-400 transition-all duration-300 transform hover:scale-110 lg:text-2xl"
-            >
-              {link.icon}
-            </a>
-          </li>
-        ))}
-      </ul>
+        <ul className="text-lg flex gap-6 items-center">
+          {socialLinks.map((link) => (
+            <li key={link.id}>
+              <a
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={link.label}
+                className="block text-slate-400 hover:text-cyan-400 transition-all duration-300 transform hover:scale-110 lg:text-2xl"
+              >
+                {link.icon}
+              </a>
+            </li>
+          ))}
+        </ul>
 
-      <div className="text-xs md:text-sm text-slate-400 text-center sm:text-right font-medium tracking-wide">
-        © {currentYear} <span className="text-white hover:text-cyan-400 transition-colors duration-300 cursor-pointer font-semibold">Khalilov Dev</span>. All rights reserved.
-      </div>
-      
+        <div className="text-xs md:text-sm text-slate-400 text-center sm:text-right font-medium tracking-wide">
+          © {currentYear}{" "}
+          <span className="text-white hover:text-cyan-400 transition-colors duration-300 cursor-pointer font-semibold">
+            Khalilov Dev
+          </span>
+          . All rights reserved.
+        </div>
       </div>
     </footer>
   );
